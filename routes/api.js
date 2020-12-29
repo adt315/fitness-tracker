90ad2ddcdd4a90c1174b8ab49f6c247ed55e16ba
adt315 <angela_dyan@hotmail.com>
2020-12-28 22:51:51 -0500

    add api routes

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,48 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
+router.post("/api/workouts", function(req, res) {
+    Workout.create(req.body)
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
 
+router.get("/api/workouts", function(req, res) {
+    Workout.find()
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
 
+router.get("/api/workouts/range", function(req, res) {
+    Workout.find()
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
 
-
+router.put("/api/workouts/:id", function(req, res) {
+    Workout.findByIdAndUpdate(
+        req.params.id,
+        {$push: {exercises: req.body}},
+        {new: true}
+    )
+    .then(dbWorkout => {
+        res.json(dbWorkout);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
 
 module.exports = router;
